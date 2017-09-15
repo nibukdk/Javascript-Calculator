cc1ef03fdd6e82311fa03f9ff4eb5c67e625634b
Nibesh Khadka <nibeshkhadka@gmail.com>
2017-09-16 01:21:37 +0300

    New changes in js and txt file addition

--- app.js
+++ app.js
@@ -3,94 +3,58 @@ $(document).ready(function() {
   var inp1, inp2;
   var operator;
   var counter = 0;
+  var digit;
+  $('.work').empty();
+  $('.work').html(0);
+  $('.result').html(0);
+
   $('.btn').on('click', function() {
-    /*counter=counter+1;
-    console.log(counter);*/
-    var digit = $(this).html();
 
-    if (digit === "AC") {
-      $('.work').empty();
+    digit = $(this).html();
+
+  });
+  //Button for Ac
+  $('.turnOn').on('click', function() {
+    $('.work').empty();
+    $('.work').html(0);
+    $('.result').html(0);
+  });
+  //Adding button cliked on screen if numbers or operators
+  $('.num , .operators, .period').on('click', function() {
+    $('.work').append(digit);
+    strings = $('.work').html();
+
+  });
+  //Making del button work
+  $('.del').on('click', function() {
+    if ($('.work').html().length <= 1) {
       $('.work').html(0);
-      $('.result').html(0);
-
-    } else if (digit === "DEL") {
-      //Incase of del key press
-      if ($('.work').html().length <= 1) {
-        $('.work').html(0);
-      } else {
-        var newString = $('.work').html().slice(0, $('.work').html().length - 1);
-        $('.work').html(newString);
-      }
-    } else if (digit === "=") {
-      strings = $('.work').html();
-      var operatorPosition = strings.search('[+\*/-]');
-
-      if (operatorPosition < 0) {
-        $('.result').html(strings);
-      } else {
-        try {
-
-          eval(strings);
-          return resultCalculation();
-        } catch (e) {
-
-          var evaluated = "Error";
-          $('.result').html(evaluated);
-        }
-
-      }
-      //Function to calculate and display final result
-      function resultCalculation() {
-        operator = strings[operatorPosition];
-        var str1 = strings.slice(0, operatorPosition);
-        var str2 = strings.slice(operatorPosition + 1, strings.length);
-        inp1 = parseFloat(str1);
-        inp2 = parseFloat(str2)
-
-        $('.result').html(calculator());
-
-      }
-
     } else {
-      $('.work').append(digit);
-      strings = $('.work').html();
-
-    }
-
-    // define  initial mode of operation
-
-    function calculator() {
-      if (operator === "+") {
-        return add();
-      } else if (operator === "-") {
-        return sub();
-      } else if (operator === "/") {
-        return div();
-      } else if (operator === "*") {
-      
-        return multiply();
-
-      } else {
-        console.log(error);
-      }
-    }
-
-    function add() {
-      return inp1 + inp2;
-    }
-
-    function sub() {
-      return inp1 - inp2;
-    }
-
-    function multiply() {
-
-      return inp1 * inp2;
-    }
-
-    function div() {
-
-      return inp1 / inp2;
+      var newString = $('.work').html().slice(0, $('.work').html().length - 1);
+      $('.work').html(newString);
     }
   });
+
+  $('.equals').on('click', function(e) {
+    e.stopPropagation();
+    resultCalculation();
+  });
+
+
+  function resultCalculation() {
+    strings = $('.work').html().slice(1, $('.work').html().length);
+    try {
+      counter = counter + 1;
+      console.log(counter);
+      var evaluation = eval(strings);
+      var correctString = $('.work').html();
+      $('.result').html(evaluation);
+
+
+    } catch (e) {
+      $('.result').html('Error');
+
+    }
+  }
+
 });
